3c46ccf6efdffe3c1e5fbfca063963ff08ef097c
Gogs <gogs@fake.local>
2016-11-21 17:45:53 +0800

    basic http server.

--- connection.hpp
+++ connection.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <boost/asio.hpp>
 #include "request.hpp"
+#include "response.hpp"
 
 namespace timax
 {
@@ -30,7 +31,7 @@ namespace timax
 	private:
 		void read_head()
 		{
-			read_buf_.consume(read_buf_.size());//清空buf
+			read_buf_.consume(read_buf_.size());
 			auto self = this->shared_from_this();
 			boost::asio::async_read_until(socket_, read_buf_, "\r\n\r\n", [this, self]
 				(const boost::system::error_code& ec, std::size_t bytes_transferred)
@@ -42,32 +43,28 @@ namespace timax
 					{
 						std::cout << "client socket shutdown" << std::endl;
 					}
-					else if (ec == boost::asio::error::not_found)
-					{
-						statas_code = 413;
-						response_and_close(statas_code, self);
-						return;
-					}
+
 					close();
 					return;
 				}
 
 				request_t request;
 				int r = request.parse(boost::asio::buffer_cast<const char*>(read_buf_.data()), bytes_transferred);
+				bool need_close = need_close_conneciton(request);
+
 				if (r < 0)
 				{
 					statas_code = 400;
-					response_and_close(statas_code, self);
+					response(statas_code, need_close, self, request.minor_version());
 					return;
 				}
 
-				bool need_close = need_close_conneciton(request);
 				size_t body_len = request.body_length();
 				if (body_len == 0)
 				{
 					if (request.has_keepalive_attr())
 					{
-						response_and_read(statas_code, need_close, self);
+						response(statas_code, need_close, self, request.minor_version());
 					}
 					else
 					{
@@ -86,7 +83,7 @@ namespace timax
 					if (body_len + bytes_transferred>8192)
 					{
 						statas_code = 413;
-						response_and_close(statas_code, self);
+						response(statas_code, need_close, self, request.minor_version());
 						return;
 					}
 					read_body(self, need_close, std::move(request), body_len);
@@ -111,22 +108,18 @@ namespace timax
 
 				try
 				{
-					//callback to user
-					//response
-					//todo
-
-					response_and_read(statas_code, need_close, self);
+					response(statas_code, need_close, self, req.minor_version());
 				}
 				catch (const std::exception& ex)
 				{
 					std::cout << ex.what() << std::endl;
 					statas_code = 400;
-					response_and_close(statas_code, self);
+					response(statas_code, need_close, self, req.minor_version());
 				}
 				catch (...)
 				{
 					statas_code = 400;
-					response_and_close(statas_code, self);
+					response(statas_code, need_close, self, req.minor_version());
 				}
 			});
 		}
@@ -162,38 +155,34 @@ namespace timax
 			socket_.close(ignored_ec);
 		}
 
-		const std::string str_ = "HTTP/1.1 200 OK\r\n"
-			"Date: Thu, 17 Nov 2016 08:38:08 GMT\r\n"
-			"Server: Apache\r\n"
-			"Content-Length: 11\r\n"
-			"Connection: Keep-Alive\r\n"
-			"Content-Type: text/html\r\n"
-			"\r\n"
-			"hello world";
-
-		//异步发送消息，响应之后如果不继续read则该连接会被关闭
-		void response_and_close(size_t status_code, const std::shared_ptr<connection>& self)
+		void response(size_t status_code, bool need_close, const std::shared_ptr<connection>& self, int minor_version = 1)
 		{
-			boost::asio::async_write(socket_, boost::asio::buffer(str_), [this, self](const boost::system::error_code& ec, std::size_t bytes_transferred)
+			auto response = std::make_shared<response_t>();
+			//callback to user
+			std::string body = "hello";
+			response->add_body(body.data(), body.size());
+			response->init_header(status_code, minor_version);
+
+			response->set_callback([this, &response](auto buffers, auto need_close)
 			{
-				if (ec != 0)
-				{
-					std::cout << ec.message() << std::endl;
-				}
-				close();
+				write(response, std::move(buffers), need_close);
 			});
+
+			response->send_response(need_close);
 		}
 
-		void response_and_read(size_t status_code, bool need_close, const std::shared_ptr<connection>& self)
+		void write(const std::shared_ptr<response_t>& response,const std::vector<boost::asio::const_buffer>& buffers, bool need_close = true)
 		{
-			boost::asio::async_write(socket_, boost::asio::buffer(str_), [this, need_close, self](const boost::system::error_code& ec, std::size_t bytes_transferred)
+			auto self = this->shared_from_this();
+			boost::asio::async_write(socket_, buffers,
+				[this, self, response, need_close](const boost::system::error_code& ec, std::size_t bytes_transferred)
 			{
 				if (ec != 0)
 				{
 					std::cout << ec.message() << std::endl;
 				}
 
-				if(need_close)
+				if (need_close)
 					close();
 				else
 					read_head();
@@ -204,19 +193,6 @@ namespace timax
 		boost::asio::ip::tcp::socket socket_;
 		boost::asio::streambuf read_buf_;
 		const int MAX_LEN = 8192;
-		const static std::map<unsigned int, std::string> http_status_table;
-	};
-
-	const std::map<unsigned int, std::string>
-		connection::http_status_table =
-	{
-		{ 200, "200 OK" },
-		{ 400, "Bad Request"},
-		{ 404, "404 Not Found" },
-		{ 413, "413 Request Entity Too Large" },
-		{ 500, "500 Server Error" },
-		{ 501, "501 Not Implemented" },
-		{ 505, "505 HTTP Version Not Supported" }
 	};
 }
 
--- response.hpp
+++ response.hpp
@@ -1,9 +1,14 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <boost/lexical_cast.hpp>
+
 namespace timax
 {
 	class response_t
 	{
 	public:
+		using Callback = std::function<void(const std::vector<boost::asio::const_buffer>&, bool)>;
 
 		response_t()
 		{
@@ -12,6 +17,83 @@ namespace timax
 		~response_t()
 		{
 		}
+
+		void init_header(unsigned int status_code, int minor_version)
+		{
+			header_str_.clear();
+			std::string protocal = "";
+			if (minor_version == 1)
+				protocal = "HTTP/1.1 ";
+			else
+				protocal = "HTTP/1.0 ";
+
+			status_line_ = protocal + HTTP_STATUS_TABLE.at(status_code) + "\r\n";
+
+			header_str_ = "Content-Length: " + boost::lexical_cast<std::string>(buffer_.size()) + "\r\n";
+
+			for (auto& iter : header_)
+			{
+				header_str_ += iter.first;
+				header_str_ += ": ";
+				header_str_ += iter.second;
+				header_str_ += "\r\n";
+			}
+
+			header_str_ += "\r\n";
+		}
+
+		void add_header(const std::string& key, const std::string& val)
+		{
+			header_.emplace(key, val);
+		}
+
+		void add_body(const char* data, size_t size)
+		{
+			buffer_.sputn(data, size); 
+		}
+
+		void send_response(bool need_close = true)
+		{
+			assert(callback_);
+
+			response_buffers_.push_back(boost::asio::buffer(status_line_));
+			response_buffers_.push_back(boost::asio::buffer(header_str_));
+			if(buffer_.size()>0)
+				response_buffers_.push_back(boost::asio::buffer(buffer_.data(), buffer_.size()));
+
+			callback_(response_buffers_, need_close);
+		}
+
+		void set_callback(Callback callback)
+		{
+			callback_ = callback;
+		}
+
+		const std::vector<boost::asio::const_buffer> get_buffers() const
+		{
+			return response_buffers_;
+		}
+
+	private:
+		std::string status_line_;
+		std::string header_str_;		
+		boost::asio::streambuf buffer_;
+		std::unordered_map<std::string, std::string> header_;
+		std::vector<boost::asio::const_buffer> response_buffers_;
+		Callback callback_;
+		const static std::unordered_map<unsigned int, std::string> HTTP_STATUS_TABLE;
+	};
+
+	const std::unordered_map<unsigned int, std::string>
+		response_t::HTTP_STATUS_TABLE =
+	{
+		{ 200, "200 OK" },
+		{ 400, "Bad Request" },
+		{ 404, "404 Not Found" },
+		{ 413, "413 Request Entity Too Large" },
+		{ 500, "500 Server Error" },
+		{ 501, "501 Not Implemented" },
+		{ 505, "505 HTTP Version Not Supported" }
 	};
 }
 
